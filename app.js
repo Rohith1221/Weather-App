de7dd5304c2a572a2840d7bf16ce2be3079e345b
Rohith1221 <62758125+Rohith1221@users.noreply.github.com>
2020-08-14 19:35:07 +0530

    update 2.js
    geocoding using callback

--- app.js
+++ app.js
@@ -15,21 +15,31 @@ request({url:url , json:true},(error, response)=>{
     console.log(chalk.green.bold(chalk.red.bold(response.body.current.weather_descriptions[0])+" The actual temp is "+chalk.cyanBright.bold(response.body.current.temperature)+" degrees F and it feels like "+chalk.bold.cyanBright(response.body.current.feelslike)+" degrees F."))
 })
 
-//geocoding
+//geocoding ...using callback
 
-const url="https://api.mapbox.com/geocoding/v5/mapbox.places/vizag.json?access_token=pk.eyJ1Ijoicm9oaXRobWFyZ2FuaSIsImEiOiJja2RrN2t2czAwbTIzMnhxM3htZXptdzluIn0.wEa3MZ0xZox2tmM0hXp3Yg&limit=1"
-request({url:url , json:true},(error,response)=>{
-    if(error)
-    {
-        console.log(chalk.red.bold("NETWORK ISSUE! "))
-    }
-    else if(response.body.features.length==0)
-    {
-        console.log(chalk.inverse.bold("Location not found"))
-    }
-    else
-    {
-        console.log(chalk.yellow.bold("Longitude :")+response.body.features[0].center[0])
-        console.log(chalk.yellow.bold("Latitude :")+response.body.features[0].center[1])
-    }
-})
+
+const geocode=(address,callback)=>{
+    const url="https://api.mapbox.com/geocoding/v5/mapbox.places/"+encodeURIComponent(address)+".json?access_token=pk.eyJ1Ijoicm9oaXRobWFyZ2FuaSIsImEiOiJja2RrN2t2czAwbTIzMnhxM3htZXptdzluIn0.wEa3MZ0xZox2tmM0hXp3Yg&limit=1"
+    request({url:url ,json:true},(error,response)=>{
+        if(error)
+        {
+            callback(chalk.red.bold("NETWORK ISSUE!",undefined))
+        }
+        else if(response.body.features.length===0)
+        {
+            callback("Location not found ",undefined)
+        }
+        else{
+            callback(undefined,{
+                Longitude: response.body.features[0].center[0],
+                Latitude: response.body.features[0].center[1],
+                Place: response.body.features[0].place_name
+        })
+        }
+    })
+}
+geocode("vizag",(error,data))
+{
+   console.log("Error",error)
+   console.log("Data",data)
+}
